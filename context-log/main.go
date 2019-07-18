8b35dcfe3bdeeaf49950c5ef7f0ccfa040d1b480
kkoehler <github@kkoehler.com>
2019-07-18 09:17:49 +0200

    Kommentare eingefügt

--- context-log/main.go
+++ context-log/main.go
@@ -10,18 +10,22 @@ func main() {
 
 	type keyval string
 
+	//Erzeugung Parent-Context
 	ctx := context.Background()
+	//hier wird der Context mit Werten erstellt
 	vctx := context.WithValue(ctx, keyval("request-id"), keyval("123"))
 
-	//Für JSON Ausgabe
+	//Für JSON Ausgabe nächste Zeile einkommentieren
 	//log.SetFormatter(&log.JSONFormatter{})
 
-	log.WithField("request-id", vctx.Value(keyval("request-id"))).Info("Output with Field")
-
+	//Logrus Logger vorkonfigurieren
 	requestLogger := log.WithFields(
 		log.Fields{
 			"request-id": vctx.Value(keyval("request-id")),
 		})
+
+	//Loggen ohne angabe weiterer Werte
+	//diese sind im Logger vorkonfiguriert
 	requestLogger.Info("Some important message")
 	requestLogger.Info("Next important message")
 
